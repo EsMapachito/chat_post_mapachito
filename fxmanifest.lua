4f2c762edaaedc4cd2ff64b4fc29d0e489638a82
Mapachito <58920957+EsMapachito@users.noreply.github.com>
2019-12-27 00:10:36 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+description 'chat_post_mapachito'
+
+fx_version "adamant"
+games {"rdr3"}
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+client_scripts {
+   'mapachito_cliente.lua'
+}
+
+server_scripts {
+'mapachito_servidor.lua'
+}
